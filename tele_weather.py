4d0a57afd69d17cb26a07317fbc5d9b4b36bdfaf
artem <artemvin007@gmail.com>
2024-02-18 11:38:23 +0300

    weather print

--- tele_weather.py
+++ tele_weather.py
@@ -1,52 +1,49 @@
-
-import pyowm  # Импортируем пакет с помощью которого мы узнаем погоду
-import telebot  # Импортируем пакет бота через ввод в CMD "pip install pytelegrambotapi"
+import pyowm #api for weather reading
+import telebot #telegram api
+from telebot import custom_filters
 import time
 
-owmToken = '0dde369d6504f5f84f2596b7fb73b966'
-owm = pyowm.OWM(owmToken)
-botToken = '6534262803:AAG8rjKkyAgZAw9qi85JsppD27TuUi_RCqI'
-bot = telebot.TeleBot(botToken)
+owm = pyowm.OWM('0dde369d6504f5f84f2596b7fb73b966')
+bot = telebot.TeleBot('6534262803:AAG8rjKkyAgZAw9qi85JsppD27TuUi_RCqI')\
+
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+    bot.send_message(message.from_user.id, "Здравствуйте. Вы можете узнать здесь погоду. Просто напишите название города." + "\n")
+    bot.set
 
 
-# Когда боту пишут текстовое сообщение вызывается эта функция
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler()
 def send_message(message):
     """Send the message to user with the weather"""
-    # Отдельно реагируем на сообщения /start и /help
+    try:
+        bot.send_message(message.chat.id, get_weather(message.text))  # Ответить сообщением
+        print(time.ctime(), "User id:", message.from_user.id)
+        print(time.ctime(), "Message:", message.text.title())
+    except Exception:
+        print(time.ctime(), "User id:", message.from_user.id)
+        print(time.ctime(), "Message:", message.text.title(), 'Error')
+        send_message(message.chat.id, "Не найден город, попробуйте ввести название снова.\n")
 
-    if message.text.lower() == "/start" or message.text.lower() == "/help":
-        bot.send_message(message.from_user.id, "Здравствуйте. Вы можете узнать здесь погоду. Просто напишите название города." + "\n")
+def get_weather(city):
+    observation = owm.weather_manager()
+    weather = observation.weather_at_place(city).weather
+    temp = weather.temperature("celsius")['temp']
+
+    answer = "В этом городе: " + str(temp) + "°C" + "\nОтносительная влажность: " + str(weather.humidity) + "%\n"
+
+    if weather.clouds < 25:
+        answer += "Безоблачно"
+    elif weather.clouds < 50:
+        answer += "Немного облачно"
+    elif weather.clouds < 75:
+        answer += "Облачно"
     else:
-        # С помощью try заставляю пройти код, если функция observation не находит город
-        # и выводит ошибку, то происходит переход к except
-        try:
-            # Имя города пользователь вводит в чат, после этого мы его передаем в функцию
-            observation = owm.weather_manager()
-            weather = observation.weather_at_place(message.text).weather
-            temp = weather.temperature("celsius")['temp']
-            print(time.ctime(), "User id:", message.from_user.id)
-            print(time.ctime(), "Message:", message.text.title(), temp, "C")
-
-            answer = "В этом городе: " + str(temp) + "°C" + "\nОтносительная влажность: " + str(weather.humidity) + "%\n"
-
-            if weather.clouds < 25:
-                answer += "Безоблачно"
-            elif weather.clouds < 50:
-                answer += "Немного облачно"
-            elif weather.clouds < 75:
-                answer += "Облачно"
-            else:
-                answer += "Пасмурно"
-
-        except Exception:
-            answer = "Не найден город, попробуйте ввести название снова.\n"
-            print(time.ctime(), "User id:", message.from_user.id)
-            print(time.ctime(), "Message:", message.text.title(), 'Error')
-
-        bot.send_message(message.chat.id, answer)  # Ответить сообщением
+        answer += "Пасмурно"
+    return answer
 
 
-# Запускаем бота
+
+
 if __name__ == __name__:
     bot.polling(none_stop=True)
