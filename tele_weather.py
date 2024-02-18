540a7a76ea08bf77b63aa888e1f62ee60fa03a01
artem <artemvin007@gmail.com>
2024-02-18 22:10:57 +0300

    it not work very good, but it work fifty fifty

--- tele_weather.py
+++ tele_weather.py
@@ -1,49 +1,120 @@
 import pyowm #api for weather reading
 import telebot #telegram api
-from telebot import custom_filters
+from telebot import types
 import time
+from datetime import datetime
+import pytz
 
-owm = pyowm.OWM('0dde369d6504f5f84f2596b7fb73b966')
-bot = telebot.TeleBot('6534262803:AAG8rjKkyAgZAw9qi85JsppD27TuUi_RCqI')\
+
+bot = telebot.TeleBot('6534262803:AAG8rjKkyAgZAw9qi85JsppD27TuUi_RCqI')
+
+class City:
+    def __init__(self, name, composition, photos):
+        self.menu = [
+            name,
+            "Узнать погоду",
+            "Узнать время",
+            "Посмотреть отели",
+            "Выбрать другой город"
+        ]
+        self.owm = pyowm.OWM('0dde369d6504f5f84f2596b7fb73b966')
+        self.name = name
+        self.composition = composition
+        self.photos = photos
+
+    def get_weather(self):
+        observation = self.owm.weather_manager()
+        weather = observation.weather_at_place(self.name).weather
+        temp = weather.temperature("celsius")['temp']
+
+        answer = "В этом городе: " + str(temp) + "°C" + "\nОтносительная влажность: " + str(
+            weather.humidity) + "%\n"
+
+        if weather.clouds < 25:
+            answer += "Безоблачно"
+        elif weather.clouds < 50:
+            answer += "Немного облачно"
+        elif weather.clouds < 75:
+            answer += "Облачно"
+        else:
+            answer += "Пасмурно"
+        return answer
+
+    def get_time(self):
+        try:
+            # Получаем текущее время
+            now = datetime.now()
+
+            # Определяем часовой пояс для введенного города
+            tz = pytz.timezone(self.name)
+
+            # Конвертируем текущее время в часовой пояс города
+            city_time = now.astimezone(tz)
+
+            # Форматируем и выводим время
+            return("Текущее время в" + self.name + ":" + city_time.strftime('%Y-%m-%d %H:%M:%S %Z'))
+
+        except pytz.UnknownTimeZoneError:
+            return("Ошибка: Часовой пояс для города" + self.name + "не найден.")
+
+
+menu = {
+    "city_choise_menu": {
+        "Горно-Алтайск": City("Горно-Алтайск",
+                              "В краю гор, лесов, бурных рек и чистейших озер до сих пор чтут традиции предков и радушно встречают путешественников. Регион ежегодно принимает порядка двух миллионов человек. Путешественники выбирают экологический, аграрный, гастрономический, познавательный или спортивный туризм, кто-то объединяет несколько направлений.Здесь находится самая высокая точка Сибири и одна из крупнейших горных вершин России — гора Белуха, или Музтау Шыны. Местные зачастую называют гору Уч-Сюре, что переводится как «жилище трех богов». Именно эта гора изображена на гербе Республики Алтай.",
+                              ["Горно-Алтайск1.jpeg", "Горно-Алтайск2.jpeg", "Горно-Алтайск3.jpeg"]),
+    },
+    "current_city": None
+}
+
+def create_markup(menu_items):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for i in (menu_items):
+        markup.add(types.KeyboardButton(i))
+    return markup
 
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.from_user.id, "Здравствуйте. Вы можете узнать здесь погоду. Просто напишите название города." + "\n")
-    bot.set
+    bot.send_message(
+        message.from_user.id, "Здравствуйте. Вы можете узнать здесь погоду. Просто напишите название города." + "\n",
+        reply_markup=create_markup(menu["city_choise_menu"].keys())
+    )
 
 
 
-@bot.message_handler()
+@bot.message_handler(content_types=["text"])
 def send_message(message):
     """Send the message to user with the weather"""
     try:
-        bot.send_message(message.chat.id, get_weather(message.text))  # Ответить сообщением
         print(time.ctime(), "User id:", message.from_user.id)
         print(time.ctime(), "Message:", message.text.title())
+        if (menu["current_city"] == None):
+            if (message.text in menu["city_choise_menu"].keys()):
+                menu["current_city"] = menu["city_choise_menu"][message.text]
+            else:
+                menu["current_city"] = City(message.text, "Это отличный город!", ["default.png"])
+
+        if (menu["current_city"] != None):
+            if (message.text == menu["current_city"].name):
+                bot.send_message(message.chat.id, menu["current_city"].name)
+                for i in menu["current_city"].photos:
+                    bot.send_photo(message.chat.id, open(i, "rb"))
+                bot.send_message(message.chat.id, menu["current_city"].composition, reply_markup=create_markup(menu["current_city"].menu))
+            if (message.text == "Узнать погоду"):
+                bot.send_message(menu["current_city"].get_weather())
+            if(message.text == "Узнать время"):
+                bot.send_message(menu["current_city"].get_time())
+            if (message.text == "Выбрать другой город"):
+                bot.send_message(message.chat.id, "Введите название другого города", reply_markup=create_markup(menu["city_choise_menu"].keys()))
+                menu["current_city"] = None
+
+
     except Exception:
         print(time.ctime(), "User id:", message.from_user.id)
         print(time.ctime(), "Message:", message.text.title(), 'Error')
-        send_message(message.chat.id, "Не найден город, попробуйте ввести название снова.\n")
-
-def get_weather(city):
-    observation = owm.weather_manager()
-    weather = observation.weather_at_place(city).weather
-    temp = weather.temperature("celsius")['temp']
-
-    answer = "В этом городе: " + str(temp) + "°C" + "\nОтносительная влажность: " + str(weather.humidity) + "%\n"
-
-    if weather.clouds < 25:
-        answer += "Безоблачно"
-    elif weather.clouds < 50:
-        answer += "Немного облачно"
-    elif weather.clouds < 75:
-        answer += "Облачно"
-    else:
-        answer += "Пасмурно"
-    return answer
-
-
+        bot.send_message(message.chat.id, "Не найден город, попробуйте ввести название снова.\n")
 
 
 if __name__ == __name__:
     bot.polling(none_stop=True)
+
